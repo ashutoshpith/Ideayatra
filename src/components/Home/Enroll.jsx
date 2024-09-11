21ec0f91be213a0c208cb5c082e48473ee906d33
ashutoshpith <amxdp18@gmail.com>
2024-09-11 23:43:16 +0530

    enroll image update2

--- src/components/Home/Enroll.jsx
+++ src/components/Home/Enroll.jsx
@@ -2,11 +2,10 @@ import EnrollForm from './EnrollForm'
 import Image from 'next/image'
 
 export default function EnrollSection() {
-  const imagePath = "/landingPage/enroll_girl.png"
     return (
         <>
         <div className="w-full h-[80px] md:h-[180px] bg-[url('../../public/landingPage/enrollBackground.png')] bg-no-repeat mt-16"></div>
-        <div className="bg-white ">
+        <div className="bg-white">
   <div className="container mx-auto px-4 flex items-center h-[calc(100vh-10rem)]">
     <div className="flex flex-col md:flex-row justify-between items-center space-y-8 md:space-y-0 md:space-x-8 w-full h-full">
       {/* Form Container */}
@@ -15,15 +14,15 @@ export default function EnrollSection() {
       </div>
 
       {/* Image Container */}
-      <div className="w-full md:w-1/2 h-full flex" >
-        <div className="relative w-full h-full">
+      <div className="hidden md:block w-full md:w-1/2 h-full relative">
+        <div className="absolute top-0 right-0 w-full h-full">
           <Image
             src="/landingPage/enroll_girl.png"
             alt="Enroll Girl"
             layout="fill"
             objectFit="cover"
-            objectPosition="center right"
-            className="rounded-lg"
+            objectPosition="right"
+            className="rounded-l-lg"
           />
         </div>
       </div>
